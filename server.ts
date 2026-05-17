2e32485bdaaa6d8ac2998c5b933b77791443286b
jvs-dev <shepherdcom12@gmail.com>
2026-05-17 03:13:55 -0300

    update sitemap

--- server.ts
+++ server.ts
@@ -18,32 +18,32 @@ async function startServer() {
     res.send(`<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
-    <loc>https://incoded.com.br/</loc>
+    <loc>https://www.incoded.com.br/</loc>
     <changefreq>daily</changefreq>
     <priority>1.0</priority>
   </url>
   <url>
-    <loc>https://incoded.com.br/contato</loc>
+    <loc>https://www.incoded.com.br/contato</loc>
     <changefreq>monthly</changefreq>
     <priority>0.8</priority>
   </url>
   <url>
-    <loc>https://incoded.com.br/blog</loc>
+    <loc>https://www.incoded.com.br/blog</loc>
     <changefreq>daily</changefreq>
     <priority>0.8</priority>
   </url>
   <url>
-    <loc>https://incoded.com.br/webdata</loc>
+    <loc>https://www.incoded.com.br/webdata</loc>
     <changefreq>weekly</changefreq>
     <priority>0.5</priority>
   </url>
   <url>
-    <loc>https://incoded.com.br/dashboard</loc>
+    <loc>https://www.incoded.com.br/dashboard</loc>
     <changefreq>weekly</changefreq>
     <priority>0.5</priority>
   </url>
   <url>
-    <loc>https://incoded.com.br/prospect</loc>
+    <loc>https://www.incoded.com.br/prospect</loc>
     <changefreq>weekly</changefreq>
     <priority>0.5</priority>
   </url>
